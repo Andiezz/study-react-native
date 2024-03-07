72fc04fd7ab96f474b4d6e2e642315b2dcc7e972
Vũ Thành An <superlegend27@gmail.com>
2024-03-07 22:22:43 +0700

    Finish section 8

--- 04-navigation copy/screens/MealDetailScreen.js
+++ 04-navigation copy/screens/MealDetailScreen.js
@@ -1,21 +1,22 @@
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { useContext, useLayoutEffect } from 'react';
+import { useLayoutEffect } from 'react';
 
 import List from '../components/MealDetail/List';
 import Subtitle from '../components/MealDetail/Subtitle';
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
 import IconButton from '../components/IconButton';
-import { FavoritesContext } from '../store/context/favorites-context';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite, removeFavorite } from '../store/redux/favorites';
 
 function MealDetailScreen({ route, navigation }) {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+  const dispatch = useDispatch();
 
   const mealId = route.params.mealId;
-
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  const mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -25,9 +26,11 @@ function MealDetailScreen({ route, navigation }) {
 
   function changeFavoriteStatusHandler() {
     if (mealIsFavorite) {
-      favoriteMealsCtx.removeFavorite(mealId);
+      // favoriteMealsCtx.removeFavorite(mealId);
+      dispatch(removeFavorite({ id: mealId }));
     } else {
-      favoriteMealsCtx.addFavorite(mealId);
+      // favoriteMealsCtx.addFavorite(mealId);
+      dispatch(addFavorite({ id: mealId }));
     }
   }
 
---
+++ 04-navigation/store/context/favorites-context.js
@@ -0,0 +1,36 @@
+import { createContext, useMemo, useState } from 'react';
+
+export const FavoritesContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+function FavoriteContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => currentFavIds.filter((mealId) => mealId !== id));
+  }
+
+  const value = useMemo(
+    () => ({
+      ids: favoriteMealIds,
+      addFavorite: addFavorite,
+      removeFavorite: removeFavorite,
+    }),
+    [favoriteMealIds]
+  );
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export default FavoriteContextProvider;
